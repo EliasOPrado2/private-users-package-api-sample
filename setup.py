e850db9c5c93ad1ad25fa2e74071cfe566f44254
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-05-10 15:33:49 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='samplemed_users_package',
+    version='0.0.1',
+    description='This is a package for instalation test.',
+    url='git@github.com:EliasOPrado2/private-users-package-api-sample.git',
+    author='Elias Prado',
+    author_email='elias.prado@samplemed.com',
+    license='samplemed',
+    packages=['samplemed_users_package'],
+    zip_safe=False
+)
